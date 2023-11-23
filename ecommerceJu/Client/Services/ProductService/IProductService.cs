22b39d485fc000d1ac393739f8f98d09e188527a
jumarino <julianamarinoupskill@outlook.com>
2023-11-23 13:13:27 +0000

    add more products, details

--- ecommerceJu/Client/Services/ProductService/IProductService.cs
+++ ecommerceJu/Client/Services/ProductService/IProductService.cs
@@ -1,11 +1,21 @@
-﻿global using ecommerceJu.Shared;
-
-namespace ecommerceJu.Client.Services.ProductService
+﻿namespace ecommerceJu.Client.Services.ProductService
 {
 	public interface IProductService
 	{
-
-		List<Product> products { get; set; }
-		Task GetProducts();
+		event Action ProductsChanged;
+		List<Product> Products { get; set; }
+		List<Product> AdminProducts { get; set; }
+		string Message { get; set; }
+		int CurrentPage { get; set; }
+		int PageCount { get; set; }
+		string LastSearchText { get; set; }
+		Task GetProducts(string? categoryUrl = null);
+		Task<ServiceResponse<Product>> GetProduct(int productId);
+		Task SearchProducts(string searchText, int page);
+		Task<List<string>> GetProductSearchSuggestions(string searchText);
+		Task GetAdminProducts();
+		Task<Product> CreateProduct(Product product);
+		Task<Product> UpdateProduct(Product product);
+		Task DeleteProduct(Product product);
 	}
 }
--- ecommerceJu/Server/Services/ProdductServices/IProductService.cs
+++ ecommerceJu/Server/Services/ProdductServices/IProductService.cs
@@ -1,10 +1,12 @@
-﻿namespace ecommerceJu.Server.Services.ProductService
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerceJu.Server.Services.ProductService
 {
 	public interface IProductService
 	{
         Task<ServiceResponse<Product>> GetProductsAsync(int productId, ServiceResponse<Product> response);
 
         Task<ServiceResponse<List<Product>>> GetProductsAsync();
-
-    }
+      
+}
 }
