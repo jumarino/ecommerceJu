1bab53321a2d6b272a63dbb43c93cc4332c43dad
jumarino <julianamarinoupskill@outlook.com>
2023-11-23 23:26:14 +0000

    users

---
+++ ecommerceJu/Shared/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecommerceJu.Shared
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public Address Address { get; set; }
+        public string Role { get; set; } = "Customer";
+    }
+}
